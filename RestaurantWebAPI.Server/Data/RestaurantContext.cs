e7de36a63bfaedc9d2f6c34a07d5dfdd86273ae7
Beata Zalewa <beata.zalewa@gmail.com>
2021-05-27 22:29:13 +0200

    Added the RestaurantContext database context

---
+++ RestaurantWebAPI.Server/Data/RestaurantContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantWebAPI.Server.Models;
+
+namespace RestaurantWebAPI.Server.Data
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext(DbContextOptions<RestaurantContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
