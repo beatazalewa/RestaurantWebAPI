9f40bed2e873b87e3f87f06dd1dce3a32ba45e15
Beata Zalewa <beata.zalewa@gmail.com>
2021-05-27 22:42:35 +0200

    Registered the database context in Startup class

--- RestaurantWebAPI.Server/Startup.cs
+++ RestaurantWebAPI.Server/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestaurantWebAPI.Server.Data;
 
 namespace RestaurantWebAPI.Server
 {
@@ -26,7 +21,8 @@ namespace RestaurantWebAPI.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<RestaurantContext>(options =>
+                options.UseInMemoryDatabase("RestaurantList"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,7 +39,6 @@ namespace RestaurantWebAPI.Server
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantWebAPI.Server v1"));
             }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -57,3 +52,6 @@ namespace RestaurantWebAPI.Server
         }
     }
 }
+
+
+
