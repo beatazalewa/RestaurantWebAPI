fdb3898c2cf710160d8618a46ed8fb372950686e
Beata Zalewa <beata.zalewa@gmail.com>
2021-05-27 22:15:54 +0200

    Added a model class Restaurant

---
+++ RestaurantWebAPI.Server/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebAPI.Server.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Display(Name = "Restaurant name:")]
+        public string Name { get; set; }
+        [Display(Name = "Address:")]
+        public string Address { get; set; }
+        [Display(Name = "Is open:")]
+        public bool IsOpen { get; set; }
+        [Display(Name = "Speciality:")]
+        public string Speciality { get; set; }
+        [Range(0, 5)]
+        [Display(Name = "Review:")]
+        public int Review { get; set; }
+    }
+}
