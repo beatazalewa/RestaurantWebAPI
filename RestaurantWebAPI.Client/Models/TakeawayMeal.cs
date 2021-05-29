79538214226cc6a3660d1315cad5a11d57dc5981
Beata Zalewa <beata.zalewa@gmail.com>
2021-05-29 08:54:13 +0200

    Added TakeawayMeal class

---
+++ RestaurantWebAPI.Client/Models/TakeawayMeal.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantWebAPI.Client.Models
+{
+    public class TakeawayMeal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
